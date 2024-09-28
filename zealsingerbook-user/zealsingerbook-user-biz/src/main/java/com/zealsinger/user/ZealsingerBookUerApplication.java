e9d86a2c63f9ecec62e44ffde65961b35f03513c
ZWH266183351 <12242836+zwh266183351@user.noreply.gitee.com>
2024-09-28 21:27:47 +0800

    feat:添加uer模块的基础搭建

---
+++ zealsingerbook-user/zealsingerbook-user-biz/src/main/java/com/zealsinger/user/ZealsingerBookUerApplication.java
@@ -0,0 +1,11 @@
+package com.zealsinger.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZealsingerBookUerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZealsingerBookUerApplication.class,args);
+    }
+}
